94cb9fb169199d7cd7cf5c71f8ebd2f54aed4cc3
unknown <lmo134051@stud.gibb.ch>
2023-01-27 10:39:55 +0100

    Added EKS Cluster

--- deployment.tf
+++ deployment.tf
@@ -153,3 +153,85 @@ resource "aws_db_instance" "chatapp-db-01" {
 //Now you have to install the Docker Services on either a ECS instance or a EKS Cluster
 
 //Enter this to get access to EKS Cluster aws eks --region us-east-1 update-kubeconfig --name [Cluster Name]
+
+
+
+resource "aws_security_group" "https" {
+  name        = "HTTPS"
+  description = "HTTPS Port 443 Open"
+  vpc_id      = aws_vpc.eks-vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+variable "eks-role-arn" {
+  type = string
+}
+
+resource "aws_eks_cluster" "eks-cluster" {
+  name = "EKS-Cluster"
+  role_arn = var.eks-role-arn
+  vpc_config {
+    subnet_ids = [aws_subnet.eks-a-1.id, aws_subnet.eks-b-1.id, aws_subnet.eks-c-1.id, aws_subnet.eks-d-1.id, aws_subnet.eks-f-1.id]
+    security_group_ids = [aws_security_group.https.id]
+  }
+}
+
+
+resource "aws_eks_node_group" "eks-cluster-node-group" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  node_group_name = "eks-cluster-node-group"
+  subnet_ids = [aws_subnet.eks-a-1.id, aws_subnet.eks-b-1.id, aws_subnet.eks-c-1.id, aws_subnet.eks-d-1.id, aws_subnet.eks-f-1.id]
+  node_role_arn = var.eks-role-arn
+  scaling_config {
+    desired_size = 5
+    max_size = 5
+    min_size = 5
+  }
+  disk_size = 20
+  instance_types = ["t3.medium"]
+  remote_access {
+    ec2_ssh_key = "eks-nodes"
+  }
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  addon_name = "kube-proxy"
+}
+
+resource "aws_eks_addon" "core_dns" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  addon_name = "core-dns"
+}
+
+resource "aws_eks_addon" "amazon_vpc_cni" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  addon_name = "amazon-vpc-cni"
+}
+
+
+resource "null_resource" "send-ps-command" {
+  provisioner "local-exec" {
+    command = "aws eks --region us-east-1 update-kubeconfig --name EKS-Cluster"
+  }
+}
