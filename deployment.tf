14e75db0bbfd9f1bd44be255d8007e03c8145759
Pablo-Wynistorf <89579477+Pablo-Wynistorf@users.noreply.github.com>
2023-02-15 12:06:58 +0100

    Update deployment.yaml
    Moin Meister!
    Diese Implementation wurde von einem echten Experten veranlasst. Diese neuen Funktionen wurden implementiert:
    -Create IAM Role EKS Cluster
    -Create IAM Role Nodegroup
    -Use Account ID to create Cluster (No more Varoiables)
    
    Besser isch es das du das i di code ine mergisch.

--- deployment.tf
+++ deployment.tf
@@ -182,14 +182,78 @@ resource "aws_security_group" "https" {
   }
 }
 
+    
+resource "aws_iam_role" "createEKSClusterRole" {
+  name = "eksClusterRole"
 
-variable "eks-role-arn" {
-  type = string
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
+
+resource "aws_iam_role" "createEKSNodeRole" {
+  name = "eksNodeRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_AmazonEKSClusterPolicyCluster" {
+  role       = aws_iam_role.createEKSClusterRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+
+resource "aws_iam_role_policy_attachment" "attach_AmazonEKSClusterPolicyNode" {
+  role       = aws_iam_role.createEKSNodeRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+
+
+resource "aws_iam_role_policy_attachment" "attach_AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.createEKSNodeRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+
+
+resource "aws_iam_role_policy_attachment" "attach_AmazonEC2ContainerRegistryReadOnlyPolicy" {
+  role       = aws_iam_role.createEKSNodeRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
+
+data "aws_caller_identity" "current" {}
+
+
+
 
 resource "aws_eks_cluster" "eks-cluster" {
   name = "eks-cluster"
-  role_arn = var.eks-role-arn
+  role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/eksClusterRole"
   vpc_config {
     subnet_ids = [aws_subnet.eks-a-1.id, aws_subnet.eks-b-1.id, aws_subnet.eks-c-1.id, aws_subnet.eks-d-1.id, aws_subnet.eks-f-1.id]
     security_group_ids = [aws_security_group.https.id]
@@ -201,7 +265,7 @@ resource "aws_eks_node_group" "eks-cluster-node-group" {
   cluster_name = aws_eks_cluster.eks-cluster.name
   node_group_name = "eks-cluster-node-group"
   subnet_ids = [aws_subnet.eks-a-1.id, aws_subnet.eks-b-1.id, aws_subnet.eks-c-1.id, aws_subnet.eks-d-1.id, aws_subnet.eks-f-1.id]
-  node_role_arn = var.eks-role-arn
+  node_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/eksNodeRole"
   scaling_config {
     desired_size = 4
     max_size = 4
